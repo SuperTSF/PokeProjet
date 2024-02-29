9ff8455922351ee232d743b6479192fc0c07701e
Thomas Simoes Ferreira <tomtomgo45@gmail.com>
2024-02-29 15:56:59 +0100

    changement d'orientation, thème, couleurs, barre d'état

--- app/src/main/java/com/example/pokeprojet/MainActivity.java
+++ app/src/main/java/com/example/pokeprojet/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
        setContentView(R.layout.accueil);
 
         Button menuButton = findViewById(R.id.menu);
@@ -109,5 +108,15 @@ public class MainActivity extends AppCompatActivity {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
         recreate(); // Recréez l'activité pour appliquer le changement de thème
+    }
+    public void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
